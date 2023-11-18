45c20d2ac1c4648d322c33c1c5f7661e4429f448
monower <sadafahmed720@gmail.com>
2023-11-18 14:02:35 +0600

    learned about react useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar"
+/* import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import { ThemeContext } from "./context/Theme/context"
 import { useState } from "react"
@@ -19,6 +19,62 @@ function App() {
         </main>
         <Footer />
       </ThemeContext.Provider>
+    </>
+  )
+}
+
+export default App
+ */
+
+import { useReducer } from "react"
+
+
+const reducer = (state, action) => {
+  const { type } = action;
+  switch (type) {
+    case 'increment': {
+      const newCount = state.count + 1;
+      const hasError = newCount > 5;
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? 'maximum reached' : null
+      };
+    }
+    case 'decrement': {
+      const newCount = state.count - 1;
+      const hasError = newCount < 0;
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? 'minimum reached' : null
+      };
+    }
+    case 'reset': {
+      return {
+        count: 0
+      }
+    }
+    default:
+      return state;
+  }
+};
+
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0
+  });
+
+  return (
+    <>
+      <p>The count is: {state.count}</p>
+      {
+        state.error && <p style={{ color: '#777777' }}>{state.error}</p>
+      }
+      <button onClick={() => dispatch({ type: 'increment' })}>increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>decrement</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>reset</button>
     </>
   )
 }
